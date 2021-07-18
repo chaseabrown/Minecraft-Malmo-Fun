6d603e08529dab2b8279966e0ce0186bbad9b6e9
chaseabrown <51271938+chaseabrown@users.noreply.github.com>
2021-07-18 12:25:13 -0500

    Add files via upload

---
+++ Bots/Collector.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 13:53:11 2021
+
+@author: chasebrown
+"""
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import numpy as np
+import random
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+class Collector Agent:
+    
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+        
+        #Used for sampling from past experiences. This is important for making sure there is enough variety in the actions
+        self.memory = deque(maxlen=2000)
+        
+        self.gamma = 0.95
+        
+        #Helps balance exploitation vs exploration
+        self.epsilon = 1.0
+        self.epsilon_decay = 0.9995
+        self.epsilon_min = 0.01
+        
+        #Step size for our optimizer
+        self.learning_rate = 0.001
+        
+        self.model = self._build_model()
+        
+    def _build_model(self):
+        
+        # Set up Model
+        model = Sequential()
+        
+        # Hidden Layers
+        model.add(Dense(24, input_dim = self.state_size, activation='relu'))
+        model.add(Dense(48, activation = 'relu'))
+        model.add(Dense(96, activation = 'relu'))
+        model.add(Dense(48, activation = 'relu'))
+        model.add(Dense(24, activation = 'relu'))
+
+        
+        # Output Layer
+        model.add(Dense(self.action_size, activation='linear'))
+        
+        #Compile Model
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        
+        return model
+    
+    #Create Datapoint for learning
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+    
+    #Determine explore or exploit
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+    
+    #Uses batch of memories to train the model
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+            
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+    
+    #Load model
+    def load(self,name):
+        self.model.load_weights(name)
+    
+    #Save Model
+    def save(self, name):
+        self.model.save_weights(name)
